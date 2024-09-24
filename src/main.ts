0a8022e96f2db55cac67d371b8bb3e31b37d904c
worjs <whworjs777@gmail.com>
2024-09-24 17:40:13 +0900

    setup class-validator & swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // class validator 세팅
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  app.enableCors();
+
+  // swagger 세팅
+  const config = new DocumentBuilder()
+    .setTitle('Eventory Server')
+    .setDescription('Eventory API description')
+    .setVersion('1.0')
+    .addTag('Eventory')
+    .build();
+
   await app.listen(3000);
 }
 bootstrap();
