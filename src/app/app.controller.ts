6df86eb962074be6900bb12ce6f304d027be53c5
worjs <whworjs777@gmail.com>
2024-09-24 17:49:02 +0900

    health check api

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @Controller()
+@ApiTags('Eventory')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('health')
+  @ApiOperation({ summary: 'Health check' })
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.healthCheck();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder } from '@nestjs/swagger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -25,6 +25,9 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Eventory')
     .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3000);
 }
