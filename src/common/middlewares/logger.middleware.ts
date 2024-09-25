6649aa532ce4112e8baa98db4a9394dcaa5b356c
worjs <whworjs777@gmail.com>
2024-09-25 23:38:56 +0900

    exception 잡아서 보여주는 filter

--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from 'express';
 import { Injectable, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
   use(request: Request, response: Response, next: NextFunction): void {
@@ -15,26 +16,20 @@ export class LoggerMiddleware implements NestMiddleware {
         responseTime: Date.now() - now,
         contentType: request.get('content-type') || request.get('Content-type'),
         accept: request.get('accept'),
-        ip: request.ip,
+        ip: request.get('x-forwarded-for') || request.ip,
         timestamp: new Date().toISOString(),
       };
     };
+
     response.on('finish', () => {
       this.apiLog(generateRequestLog());
     });
-    response.on('error', () => {
-      this.apiError(generateRequestLog());
-    });
+
     next();
   }
+
   private apiLog(message: object): void {
     const level = 'API_REQUEST';
     console.log(JSON.stringify({ level, ...message }));
-  }
-
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  private apiError(message: object): void {
-    const level = 'API_ERROR';
-    console.error(JSON.stringify({ level, ...message }));
   }
 }
