65523cb9138244d33134c7fd63f4c76d75711164
worjs <whworjs777@gmail.com>
2024-09-25 23:31:44 +0900

    logger setting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModule } from './modules/config.module';
+import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
 
 @Module({
   imports: [configModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
