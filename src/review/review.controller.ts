a158d27619ab87e77e6e80c221c28e3ab5ff7400
worjs <whworjs777@gmail.com>
2024-10-05 14:58:04 +0900

    controller tag 빼먹었다...

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -1,10 +1,11 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ReviewService } from './review.service';
-import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ReviewDto } from './dto/review.dto';
 import { CreateReviewPayload } from './payload/create-review.payload';
 
 @Controller('reviews')
+@ApiTags('Review API')
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
