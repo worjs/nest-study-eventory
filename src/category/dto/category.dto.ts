29830f5e113fa102c671a4a5e1ab7195332ec544
Adina Meiremkhankyzy <adinameiremkhankyzy@Adinas-MacBook-Pro.local>
2024-10-14 17:48:38 +0900

    Update category.dto.ts

--- src/category/dto/category.dto.ts
+++ src/category/dto/category.dto.ts
@@ -29,12 +29,7 @@ export class CategoryDto {
 export class CategoryListDto {
   @ApiProperty({
     description: '카테고리 목록',
-<<<<<<< HEAD
-    type: [CategoryDto],
-=======
-    type: [CategoryDto]
->>>>>>> bdbee3c (Category folder changed)
-    type: [CategoryDto]
+        type: [CategoryDto]
   })
   categories!: CategoryDto[];
 
