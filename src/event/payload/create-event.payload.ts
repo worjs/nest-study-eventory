4db4a667b1a9d89dce2abce4c6ec4be1b968d4ac
yeeun <103231425+corgiLoaf@users.noreply.github.com>
2024-10-23 15:40:46 +0900

    feat: payload 추가

---
+++ src/event/payload/create-event.payload.ts
@@ -0,0 +1,61 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsInt, IsDate, Min } from 'class-validator';
+
+export class CreateEventPayload {
+  @IsInt()
+  @ApiProperty({
+    description: '호스트 ID',
+    type: Number,
+  })
+  hostId!: number;
+
+  @IsString()
+  @ApiProperty({
+    description: '이벤트 제목',
+    type: String,
+  })
+  title!: string;
+
+  @IsString()
+  @ApiProperty({
+    description: '이벤트 설명',
+    type: String,
+  })
+  description!: string;
+
+  @IsInt()
+  @ApiProperty({
+    description: '카테고리 ID',
+    type: Number,
+  })
+  categoryId!: number;
+
+  @IsInt()
+  @ApiProperty({
+    description: '도시 ID',
+    type: Number,
+  })
+  cityId!: number;
+
+  @IsDate()
+  @ApiProperty({
+    description: '시작 시간',
+    type: Date,
+  })
+  startTime!: Date;
+
+  @IsDate()
+  @ApiProperty({
+    description: '종료 시간',
+    type: Date,
+  })
+  endTime!: Date;
+
+  @IsInt()
+  @Min(1)
+  @ApiProperty({
+    description: '최대 인원',
+    type: Number,
+  })
+  maxPeople!: number;
+}
